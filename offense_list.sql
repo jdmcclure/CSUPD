70fd97c7ba1d3f4e2160b65cf456b78024e7320a
jdmcclure <trashmail.mcclure@gmail.com>
2024-04-09 13:22:56 -0600

    Adding column alias names

--- offense_list.sql
+++ offense_list.sql
@@ -5,8 +5,8 @@ I use this to ensure the clery_offense_list is up to date with violation code.
 I would recommend exporting to Excel to search for keywords in the description.
 */
 SELECT
-    ViolationCodeReference_Code
-    ,ViolationCodeReference_Description
+    ViolationCodeReference_Code AS 'code'
+    ,ViolationCodeReference_Description AS 'description'
 FROM
     InformRMSSummaries.Reporting.IncidentOffense
 GROUP BY
